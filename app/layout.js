3a8ade9c1e83def005d7d59543963fe7ac18e8b5
Patrick Lemmer <patrick.lemmer@yahoo.com>
2023-07-18 19:50:07 +0200

    added UI components for documentation

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -13,6 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`bg-white ${inter.className}`}>
+        <div className="max-w-3xl mx-auto px-6 py-12">
+          <Sidebar />
+        </div>
         <div className="max-w-3xl mx-auto">
           <Navbar />
           <div className="mt-8">{children}</div>
---
+++ components/Sidebar.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import React from 'react';
+import { Card, Text, Button } from '@nextui-org/react';
+import { HiOutlineTrash, HiPencilAlt } from 'react-icons/hi';
+
+function Sidebar() {
+  return (
+    <Card
+      css={{
+        background: '#f7f7f7',
+        color: '#333',
+        paddingBlock: '4px',
+        paddingInline: '16px',
+      }}
+    >
+      <Card.Body>
+        <Text className="mb-5">
+          This demo app has full CRUD functionality. You can add, view, update
+          and delete topics in the list below. All topics are stored in and
+          retrieved from a MongoDB database.
+        </Text>
+        <Text className="flex gap-2 mb-3 items-center">
+          Click
+          <Button
+            css={{
+              px: '$6',
+              py: '$8',
+              color: '#333',
+              background: '#eee',
+              borderRadius: '3px',
+            }}
+            size="xs"
+          >
+            Add a topic
+          </Button>
+          to add a new topic.
+        </Text>
+        <Text className="flex gap-2 items-center mb-3">
+          Click
+          <HiOutlineTrash size={20} className="text-red-400" />
+          to delete a topic.
+        </Text>
+        <Text className="flex gap-2 items-center">
+          Click
+          <HiPencilAlt size={20} />
+          to update a topic.
+        </Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Sidebar;
